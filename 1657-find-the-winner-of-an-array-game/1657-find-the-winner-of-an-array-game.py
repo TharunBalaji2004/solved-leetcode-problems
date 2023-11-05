b6e140788e506e1dae267f67eff5b0f76d09d75d
Tharun Balaji R <95350584+TharunBalaji2004@users.noreply.github.com>
2023-11-05 09:17:02 +0530

    Time: 517 ms (80.98%), Space: 29.5 MB (94.02%) - LeetHub

--- 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.py
+++ 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.py
@@ -1,25 +1,18 @@
 from collections import defaultdict
 class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
-        d = defaultdict(int)
-        if (k >= len(arr)):
-            return max(arr)
-        while (True):
-            val1 = arr[0]
-            val2 = arr[1] 
-            winner, loser = [], []
+        curr = arr[0]
+        win = 0
 
-            if (val1 > val2):
-                winner = [0, val1]
-                loser = [1, val2]
-            else:
-                winner = [1, val2]
-                loser = [0, val1]
-            
-            d[winner[1]] += 1
-            if (d[winner[1]] == k):
-                return winner[1]
+        for i in range(1, len(arr)):
+            if (arr[i] > curr):
+                curr = arr[i]
+                win = 0
+                
+            win += 1
 
-            arr.pop(loser[0])
-            arr.append(loser[1])
+            if (win == k):
+                break
+
+        return curr
             
