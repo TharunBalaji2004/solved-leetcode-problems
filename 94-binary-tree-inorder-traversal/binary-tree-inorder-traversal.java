425b22330eab8cd8da04eca55ea9d48e922412d7
Tharun Balaji R <95350584+TharunBalaji2004@users.noreply.github.com>
2024-04-15 19:36:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (84.80%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -14,19 +14,47 @@
  * }
  */
 class Solution {
-    // TC: O(N) and SC: O(N)
-    // Inorder - L root R
-    public void helper(TreeNode root, List<Integer> list) {
-        if (root == null) return;
-        
-        helper(root.left, list);
-        list.add(root.val);
-        helper(root.right, list);
-    }
-
+    // Morris Inorder Traversal
+    // TC: O(N) and SC: O(1)
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        helper(root, list);
+        TreeNode curr = root;
+
+        while (curr != null) {
+            // Case 1 - When the current node does not have left node
+            // Action - Add the element to list and move to right
+            // Reason - Since the left is null, we cannot traverse to left
+            if (curr.left == null) {
+                list.add(curr.val);
+                curr = curr.right;
+            } 
+            // Case 2 - When the current node have left node
+            // Action - Create prev pointer and traverse till the right most
+            // Reason - To establish connection from right most node to curr (parent)
+            else {
+                TreeNode prev = curr.left;
+                while (prev.right != null && prev.right != curr) {
+                    prev = prev.right;
+                }
+
+                // Case 2.1 - When the right most node is null
+                // Action - Establish the connection from right most node to parent (prev.right = curr)
+                // Reason - To perform traversal afterwards and now move curr to left
+                if (prev.right == null) {
+                    prev.right = curr;
+                    curr = curr.left;
+                } 
+                // Case 2.2 - When the right most node is not null
+                // Action - Cut the connection with the parent node and add the parent (curr) node
+                // Reason - Since the left subtree has been already traversed
+                else {
+                    prev.right = null;
+                    list.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+        }
+
         return list;
     }
 }
