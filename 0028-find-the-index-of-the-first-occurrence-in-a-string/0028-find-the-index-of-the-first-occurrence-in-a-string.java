b609621450a463c702bad51a7059d26d5a1c23a5
Tharun Balaji R <95350584+TharunBalaji2004@users.noreply.github.com>
2024-08-13 21:21:38 +0530

    Time: 0 ms (100%), Space: 41.4 MB (59.23%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,13 +1,48 @@
 class Solution {
-    public int strStr(String string, String pattern) {
-        int n = string.length(), m = pattern.length();
+    public int[] helper(String pattern) {
+        int m = pattern.length();
+        int[] lps = new int[m];
 
-        for (int i=0;i<=n-m;i++) {
-            String window = string.substring(i, i+m);
+        int prevLPS = 0, i = 1;
 
-            if (window.equals(pattern)) return i;
+        while (i < m) {
+            if (pattern.charAt(i) == pattern.charAt(prevLPS)) {
+                lps[i] = prevLPS + 1;
+                i++;
+                prevLPS++;
+            } else {
+                if (prevLPS == 0) {
+                    lps[i] = 0;
+                    i++;
+                } else {
+                    prevLPS = lps[prevLPS - 1];
+                }         
+            }
         }
 
-        return -1;   
+        return lps;
+    }
+
+    public int strStr(String string, String pattern) {
+        int[] lps = helper(pattern);  
+
+        int i = 0, j = 0, m = pattern.length();
+
+        while (i < string.length()) {
+            if (string.charAt(i) == pattern.charAt(j)) { // When character in string and pat matches
+                i++;
+                j++;
+            } else { // When character does not matches
+                if (j == 0) i++;
+                else j = lps[j-1];
+            }
+
+
+            if (j == m){ // When pattern is found
+                return i-m;
+            } 
+        }
+
+        return -1;
     }
 }
