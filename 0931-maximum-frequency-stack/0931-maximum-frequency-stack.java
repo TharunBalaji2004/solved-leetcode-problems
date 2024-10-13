317f970f42e14c2ee8ff43d39156f56931114cb1
Tharun Balaji R <95350584+TharunBalaji2004@users.noreply.github.com>
2024-10-13 21:21:51 +0530

    Time: 41 ms (23.26%), Space: 57 MB (17.86%) - LeetHub

--- 0931-maximum-frequency-stack/0931-maximum-frequency-stack.java
+++ 0931-maximum-frequency-stack/0931-maximum-frequency-stack.java
@@ -1,35 +1,30 @@
 class FreqStack {
-
-    Map<Integer, Integer> map;
-    PriorityQueue<int[]> q;
-    int id;
+    // Declaration
+    HashMap<Integer, Integer> map;
+    PriorityQueue<int[]> pq;
+    int index;
 
     public FreqStack() {
         map = new HashMap<>();
-        id = 0;
-        q = new PriorityQueue<>((a, b) -> {
-            if(b[1] != a[1]) return b[1] - a[1];    
-            else return b[2] - a[2];
-        });
+        pq = new PriorityQueue<>(
+            (a, b) -> {
+                if (b[1] != a[1]) return b[1] - a[1];
+                else return b[2] - a[2];
+            }
+        );
+        index = 0;
     }
     
     public void push(int val) {
         int freq = map.getOrDefault(val, 0) + 1;
         map.put(val, freq);
-        id++;
-        q.add(new int[]{val, freq, id});
+        index++;
+        pq.add(new int[]{val, freq, index});
     }
     
     public int pop() {
-        int[] max = q.poll();
-        map.put(max[0], max[1]-1);
-        return max[0];
+        int[] arr = pq.poll();
+        map.put(arr[0], arr[1] - 1);
+        return arr[0];
     }
 }
-
-/**
- * Your FreqStack object will be instantiated and called as such:
- * FreqStack obj = new FreqStack();
- * obj.push(val);
- * int param_2 = obj.pop();
- */
