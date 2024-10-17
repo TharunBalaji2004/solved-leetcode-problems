6bc73d4abe21e58a05e09b1f62d433851c3a31df
Tharun Balaji R <95350584+TharunBalaji2004@users.noreply.github.com>
2024-10-17 21:40:09 +0530

    Time: 0 ms (100%), Space: 44.4 MB (5.96%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -8,14 +8,10 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-        // CASE 1: Whether node is last node
-        if (node.next == null) node = null;
-        // CASE 2: When node is not the last node
-        else {
-            // Copying the next value 
-            int temp = node.next.val;
-            node.val = temp;
-
+        if (node.next == null) {
+            node = null;
+        } else {
+            node.val = node.next.val;
             node.next = node.next.next;
         }
     }
